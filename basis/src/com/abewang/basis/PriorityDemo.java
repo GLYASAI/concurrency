aca473e52e73119f7660b459899167f98e7a2444
GLYASAI <chaoschunqfeijg@163.com>
2018-04-19 21:39:24 +0800

    线程优先级

---
+++ basis/src/com/abewang/basis/PriorityDemo.java
@@ -0,0 +1,50 @@
+package com.abewang.basis;
+
+/**
+ * 线程优先级
+ *
+ * @Author Abe
+ * @Date 2018/4/19.
+ */
+public class PriorityDemo {
+    public static class HighPriority extends Thread {
+        int count = 0;
+
+        public void run() {
+            while (true) {
+                synchronized (PriorityDemo.class) {
+                    count++;
+                    if (count > 10000) {
+                        System.out.println("HighPriority complete.");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public static class LowPriority extends Thread {
+        int count = 0;
+
+        public void run() {
+            while (true) {
+                synchronized (PriorityDemo.class) {
+                    count++;
+                    if (count > 10000) {
+                        System.out.println("LowPriority complete.");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        HighPriority highPriority = new HighPriority();
+        LowPriority lowPriority = new LowPriority();
+        highPriority.setPriority(Thread.MAX_PRIORITY);
+        lowPriority.setPriority(Thread.MIN_PRIORITY);
+        highPriority.start();
+        lowPriority.start();
+    }
+}
