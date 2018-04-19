93a3e8509a389e3d103777bf3508f2073e11da42
GLYASAI <chaoschunqfeijg@163.com>
2018-04-19 22:28:06 +0800

    线程安全的概念与synchronized

---
+++ basis/src/com/abewang/basis/AccountingSync.java
@@ -0,0 +1,31 @@
+package com.abewang.basis;
+
+/**
+ * @Author Abe
+ * @Date 2018/4/19.
+ */
+public class AccountingSync implements Runnable {
+    private static AccountingSync instance = new AccountingSync();
+    private static int i;
+
+    public void run() {
+        for (int j = 0; j < 10000000; j++) {
+            synchronized (instance) {
+                i++;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(instance);
+        t1.setName("t1");
+        Thread t2 = new Thread(instance);
+        t1.setName("t2");
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        System.out.println(i);
+    }
+}
---
+++ basis/src/com/abewang/basis/AccountingVol.java
@@ -0,0 +1,29 @@
+package com.abewang.basis;
+
+/**
+ * @Author Abe
+ * @Date 2018/4/19.
+ */
+public class AccountingVol implements Runnable {
+    private static AccountingVol instance = new AccountingVol();
+    private static volatile int i;
+
+    public void run() {
+        for (int j = 0; j < 10000000; j++) {
+            i++;
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(instance);
+        t1.setName("t1");
+        Thread t2 = new Thread(instance);
+        t1.setName("t2");
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        System.out.println(i);
+    }
+}
