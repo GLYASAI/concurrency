161913abe4fabfd4d8fde027c4c687799329960c
GLYASAI <chaoschunqfeijg@163.com>
2018-04-19 20:39:24 +0800

    线程组

---
+++ basis/src/com/abewang/basis/ThreadGroupName.java
@@ -0,0 +1,30 @@
+package com.abewang.basis;
+
+/**
+ * 线程组
+ *
+ * @Author Abe
+ * @Date 2018/4/19.
+ */
+public class ThreadGroupName implements Runnable {
+    public void run() {
+        String groupName = Thread.currentThread().getThreadGroup().getName();
+        String name = Thread.currentThread().getName();
+        System.out.println("I am " + groupName + "-" + name);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ThreadGroup tg = new ThreadGroup("Java并发程序设计");
+        Thread foo = new Thread(tg, new ThreadGroupName(), "foo");
+        Thread bar = new Thread(tg, new ThreadGroupName(), "bar");
+        foo.start();
+        bar.start();
+        System.out.println(tg.activeCount());
+        tg.list();
+    }
+}
