bc62822ba0ef00f8a00cecfb0092b0dd09980dc8
GLYASAI <chaoschunqfeijg@163.com>
2018-04-19 21:28:26 +0800

    守护线程

---
+++ basis/src/com/abewang/basis/DaemonT.java
@@ -0,0 +1,32 @@
+package com.abewang.basis;
+
+/**
+ * 守护线程
+ *
+ * @Author Abe
+ * @Date 2018/4/19.
+ */
+public class DaemonT implements Runnable {
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("I am alive.");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread dt = new Thread(new DaemonT());
+        dt.setName("Daemon Thread");
+        // 设置为守护线程后, dt会在main线程结合后结束
+        // 否则会一直执行下去
+        dt.setDaemon(true);
+        dt.start();
+
+        Thread.sleep(2000);
+    }
+}
