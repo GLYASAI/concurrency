2dd85f5a9f386b57fddae2e60445d6396e6a4059
GLYASAI <chaoschunqfeijg@163.com>
2018-04-19 08:07:45 +0800

    volatile与JMM

---
+++ basis/src/com/abewang/basis/BadAtomicity.java
@@ -0,0 +1,40 @@
+package com.abewang.basis;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * volatile无法保证复合操作的原子性
+ *
+ * @Author Abe
+ * @Date 2018/4/18.
+ */
+public class BadAtomicity {
+    public static volatile int i;
+
+    public static class PlusTask implements Runnable {
+        @Override
+        public void run() {
+            for (int j = 0; j < 10000; j++) {
+                i++;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        List<Thread> threadList = new ArrayList<>();
+
+        for (int j = 0; j < 10; j++) {
+            Thread thread = new Thread(new PlusTask());
+            threadList.add(thread);
+            thread.start();
+        }
+
+        for (Thread thread : threadList) {
+            thread.join();  // 等待threadList中的线程跑完
+        }
+
+
+        System.out.println(i);  // i小于100000，说明volatile无法保证i++的原子性
+    }
+}
---
+++ basis/src/com/abewang/basis/NoVisibility.java
@@ -0,0 +1,29 @@
+package com.abewang.basis;
+
+/**
+ * volatile保证数据的可见性
+ *
+ * @Author Abe
+ * @Date 2018/4/18.
+ */
+public class NoVisibility {
+    public static int number;
+    public static volatile boolean ready;
+
+    public static class ReaderThread extends Thread {
+
+        public void run() {
+            while (!ready);
+            System.out.println(number);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread readerThread = new ReaderThread();
+        readerThread.setName("ReaderThread");
+        readerThread.start();
+        Thread.sleep(1000);  // ?? 为什么加了sleep，ReaderThread就看不到ready的变化(没加volatile的前提)
+        number = 300;
+        ready = true;
+    }
+}
