d7abbd39a78634788636324f9019cc6bff9bb34b
GLYASAI <chaoschunqfeijg@163.com>
2018-04-24 23:08:29 +0800

    修改包名

---
+++ basis/src/orgabewang/basis/ArrayListMultiThread.java
@@ -0,0 +1,29 @@
+package com.abewang.basis;
+
+import java.util.ArrayList;
+
+/**
+ * @Author Abe
+ * @Date 2018/4/19.
+ */
+public class ArrayListMultiThread implements Runnable {
+    private static ArrayList<Integer> list = new ArrayList<>();
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000000; i++) {
+            list.add(i);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(new ArrayListMultiThread());
+        Thread t2 = new Thread(new ArrayListMultiThread());
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        System.out.println(list.size());
+    }
+}
